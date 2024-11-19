a424a2da689a3419307eb69de2191b06a34bf328
miao-hq <1633049666@qq.com>
2024-11-19 11:29:20 +0800

    update

--- amplify/data/getTest.js
+++ amplify/data/getTest.js
@@ -1,7 +1,7 @@
 import * as ddb from "@aws-appsync/utils/dynamodb";
 
 export function request(ctx) {
-  return ddb.get({ key: { id: ctx.args.id } });
+  return ddb.get({ key: { id: ctx.args.id,sort:6 } });
 }
 
 export const response = (ctx) => ctx.result;
