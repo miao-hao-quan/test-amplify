16c5c96ce2c646a7191b364672037202be78cf4e
miao-hq <1633049666@qq.com>
2024-11-20 17:17:24 +0800

    update

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
-import "./assets/main.css";
+// import "./assets/main.css";
 import { createApp } from "vue";
+import Antd from 'ant-design-vue';
 import App from "./App.vue";
 import { Amplify } from "aws-amplify";
 import outputs from "../amplify_outputs.json";
+import 'ant-design-vue/dist/reset.css';
 
 Amplify.configure(outputs);
 
-createApp(App).mount("#app");
+const app = createApp(App);
+
+app.use(Antd).mount('#app')
